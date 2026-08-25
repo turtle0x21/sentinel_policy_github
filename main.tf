f80cef4d21db7824f8146d900c7f54d6803f63ea
Pavan Kalyan <pavanreddy4680@gmail.com>
2026-08-25 15:59:13 +0530

    aws security group

--- main.tf
+++ main.tf
@@ -19,11 +19,48 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# ==========================================================
+# EC2 INSTANCE
+# ==========================================================
+
 resource "aws_instance" "sentinel_test" {
-  ami           = "YOUR_AP_SOUTH_1_AMI"
+  ami           = "YOUR_AMI_ID"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "sentinel-github-test"
+    Name        = "sentinel-ec2-test"
+    Environment = "practice"
+    Owner       = "terraform"
+  }
+}
+
+# ==========================================================
+# SECURITY GROUP
+# ==========================================================
+
+resource "aws_security_group" "sentinel_test_sg" {
+  name        = "sentinel-test-sg"
+  description = "Security group for Sentinel testing"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "sentinel-test-sg"
+    Environment = "practice"
   }
 }
