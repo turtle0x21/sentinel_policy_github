8c3eb381c3aec2347a3b7c49aad3fbd764c0f0a6
Pavan Kalyan <pavanreddy4680@gmail.com>
2026-08-25 16:40:52 +0530

    automatically trigger

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_security_group" "sentinel_test_sg" {
     to_port     = 22
     protocol    = "tcp"
 
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -56,7 +56,7 @@ resource "aws_security_group" "sentinel_test_sg" {
     to_port     = 0
     protocol    = "-1"
 
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
